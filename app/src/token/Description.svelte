34f9953bff11bac1d0a98a03fd31057f489ad47e
user <user@localhost>
2021-11-22 12:08:19 -0500

    token description design improvements

--- app/src/token/Description.svelte
+++ app/src/token/Description.svelte
@@ -1,7 +1,7 @@
 <div id="ui">
 	<h1>What is the BRIB Token?</h1>
 
-	<p>BRIB is a community token for the Brisket DAO. The token:</p>
+	<p>🪙 BRIB is a community token for the Brisket DAO. The token:</p>
 
 	<ul>
 		<li>is used to reward contributions to the DAO.</li>
@@ -14,71 +14,77 @@
 				<li>
 					voting and influence on the future direction of the DAO.
 				</li>
-				<li>incentivize, encourage, and support Brisket DAO community members to develop their own new crypto projects.</li>
-				<li>positively influence the future direction of crypto projects in general.</li>
+				<li>
+					incentivize, encourage, and support Brisket DAO community
+					members to develop their own new crypto projects.
+				</li>
+				<li>
+					positively influence the future direction of crypto projects
+					in general.
+				</li>
 			</ul>
 		</li>
 	</ul>
 
 	<p>
-
-		The Brisket DAO is a <strong>community, not an investment</strong> or
-		profit sharing scheme. The token is not used to raise capital or to
-		promise any kind of return for holders.
-		
+		The Brisket DAO is a <strong>community, not an investment</strong> or profit
+		sharing scheme. The token is not used to raise capital or to promise any
+		kind of return for holders.
 	</p>
 
-	<h2>How to get BRIB</h2>
+	<div class="content-card">
+		<h2>🔨 How to get BRIB</h2>
 
-	<ul>
-		<li>
-			The primary way to get BRIB is to <a
-				href="/#/contributions/suggested"
-				>earn it by contributing to the DAO</a
-			>.
-		</li>
-		<li>
-			BRIB can also be bought and sold via the <a
-				href="https://traderjoexyz.com/#/trade?inputCurrency=0x130966628846bfd36ff31a822705796e8cb8c18d&outputCurrency=0xa1437720c93b791b72f5b8a5846227763792afd7"
-				>BRIB/MIM pair on Trader Joe</a
-			>.
-		</li>
-	</ul>
+		<ul>
+			<li>
+				The primary way to get BRIB is to <a
+					href="/#/contributions/suggested"
+					>earn it by contributing to the DAO</a
+				>.
+			</li>
+			<li>
+				BRIB can also be bought and sold via the <a
+					href="https://traderjoexyz.com/#/trade?inputCurrency=0x130966628846bfd36ff31a822705796e8cb8c18d&outputCurrency=0xa1437720c93b791b72f5b8a5846227763792afd7"
+					>BRIB/MIM pair on Trader Joe</a
+				>.
+			</li>
+		</ul>
+	</div>
 
-	<h2>Tokenomics</h2>
+	<div class="content-card">
+		<h2>🖩 Tokenomics</h2>
 
-	<p>BRIB has no max supply currently. Current supply and stats are available on the <a href="/#/token">token page</a>.</p>
 
-	<h3>Inflationary Period (2022-2027)</h3>
+		<p>
+			BRIB has no max supply currently. Current supply and stats are
+			available on the <a href="/#/token">token page</a>.
+		</p>
 
-	<p>
-		
-		During the early stages of the community, the token supply will increase
-		in order to reward community contributions.
-	
-	</p>
+		<h3>Inflationary Period (2022-2027)</h3>
 
-	<p>
+		<p>
+			During the early stages of the community, the token supply will
+			increase in order to reward community contributions.
+		</p>
 
-		Contributions are tracked and vetted by community members and rewarded
-		retroactively. Community members who stick around and consistently
-		contribute will be given power to reward others for their contributions.
-		
-	</p>
-		
-	<h3>Stabilization Period (2027+)</h3>
+		<p>
+			Contributions are tracked and vetted by community members and
+			rewarded retroactively. Community members who stick around and
+			consistently contribute will be given power to reward others for
+			their contributions.
+		</p>
 
-	<p>
+		<h3>Stabilization Period (2027+)</h3>
 
-		As the community grows and stabilizes in its structure, token emissions
-		will decrease.
-		
-	</p>
+		<p>
+			As the community grows and stabilizes in its structure, token
+			emissions will decrease.
+		</p>
 
-	<h3>Eventual Max Supply</h3>
+		<h3>Eventual Max Supply</h3>
 
-	<p>
-		The eventual max supply of BRIB will be determined by the community.
-	</p>
-
+		<p>
+			The eventual max supply of BRIB will be determined by the community.
+		</p>
+	</div>
 </div>
